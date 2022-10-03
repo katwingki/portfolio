ad0100fe7fe97b05bc51a3dd5e1c7be5817f55c8
Kathryn Lam <katwingki@gmail.com>
2022-10-02 21:39:19 -0400

    updated

---
+++ src/config/keys.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBMhNBX-Ti2TKUZV2br2k-yIuEi4UhBBQ8",
+  authDomain: "portfolio-3be27.firebaseapp.com",
+  projectId: "portfolio-3be27",
+  storageBucket: "portfolio-3be27.appspot.com",
+  messagingSenderId: "836866231956",
+  appId: "1:836866231956:web:7c5676f1427a6e3e2d2e81",
+  measurementId: "G-WKR10BQLN7"
+};
+export default firebaseConfig;
